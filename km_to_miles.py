0e84ebd93a7b91902d7093f574f892477da683d8
davor <davor.kobescak@gmail.com>
2022-12-20 20:54:35 +0100

    izmjena na km to miles

--- km_to_miles.py
+++ km_to_miles.py
@@ -3,11 +3,19 @@ def km_to_miles():
     print()
     km_or_miles = input("Zelis li pretvoriti kilometre u milje (KM) ili milje u kilometre (MILJE)? Unesi KM ili MILJE: ").lower()
     print()
-    distance = int(input("Unesi distancu za preracunati: "))
-    print()
-    if km_or_miles == "milje":
-        kilometers = distance / factor
-        print(f"U {distance} milja ima {kilometers:.2f} kilometara")
-    else:
-        miles = distance * factor
-        print(f"U {distance} kilometara ima {miles:.2f} milja")
+    if km_or_miles != "km" and km_or_miles != "milje":
+        print("Krivi unos jedinice za konverziju. Pokušajte ponovno.")
+        km_to_miles()
+    try:
+        distance = int(input("Unesi distancu za preracunati: "))
+        print()
+        if km_or_miles == "milje":
+            kilometers = distance / factor
+            print(f"U {distance} milja ima {kilometers:.2f} kilometara")
+        elif km_or_miles == "km":
+            miles = distance * factor
+            print(f"U {distance} kilometara ima {miles:.2f} milja")
+
+    except ValueError:
+        print("Nije unesen broj. Pokusajte ponovno.")
+        km_to_miles()
